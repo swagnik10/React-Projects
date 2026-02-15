402d7dc48d61dc8c14328c8443f7fc45e3b1150a
swagnik10 <swagnikghosh07@gmail.com>
2026-02-15 13:38:08 +0530

    Theme Toggler project

---
+++ ThemeToggle/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='p-4 bg-pink-600'>Hello World</h1> */}
+
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+              {/* themebtn */}
+          <div className="w-full max-w-sm mx-auto"></div>
+              {/* cardbtn */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
