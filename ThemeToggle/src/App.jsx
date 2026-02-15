4c165732ab1d38188de9bcea064e328821af7d45
swagnik10 <swagnikghosh07@gmail.com>
2026-02-15 14:38:40 +0530

    background not changing

--- ThemeToggle/src/App.jsx
+++ ThemeToggle/src/App.jsx
@@ -1,20 +1,43 @@
 import { useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./context/theme";
+import { useEffect } from "react";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  //actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
     <>
       {/* <h1 className='p-4 bg-pink-600'>Hello World</h1> */}
 
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-              {/* themebtn */}
-          <div className="w-full max-w-sm mx-auto"></div>
-              {/* cardbtn */}
+      <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+                {/* themebtn */}
+                <ThemeBtn />
+            <div className="w-full max-w-sm mx-auto"></div>
+                {/* cardbtn */}
+                <Card />
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
